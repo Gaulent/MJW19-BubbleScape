14305ca4b60ba7c36c124c07edbc5b64766f4804
Gaulent <jose.saucod@gmail.com>
2025-01-25 20:38:45 +0100

    Cerrando mecanicas

--- scripts/bubble_spawn_point.gd
+++ scripts/bubble_spawn_point.gd
@@ -1,11 +1,21 @@
 extends Node2D
 
 @onready var spawn_timer:Timer = $Timer
+@onready var small_bubble_timer:Timer = $SmallBubbleTimer
 @export var bubble_scene:PackedScene
+@export var small_bubble_scene:PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	spawn_timer.timeout.connect(spawn_bubble)
+	small_bubble_timer.timeout.connect(spawn_small_bubble)
+	
+
+	
+
+
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,3 +24,11 @@ func spawn_bubble() -> void:
 	
 	add_child(current_bubble)
 	current_bubble.global_position = global_position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func spawn_small_bubble() -> void:
+	var current_bubble:Node2D = small_bubble_scene.instantiate()
+
+	add_child(current_bubble)
+	current_bubble.global_position = global_position
+	small_bubble_timer.start(randf_range(0.5, 2))
