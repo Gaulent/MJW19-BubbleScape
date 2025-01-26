e15905a4eb09a7a33fb888e5bd3a2b44c9f93114
Gaulent <jose.saucod@gmail.com>
2025-01-26 12:46:48 +0100

    Gameplay Actualizado

---
+++ scripts/coin.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@onready var audio:AudioStreamPlayer = $AudioStreamPlayer
+@onready var sprite:AnimatedSprite2D = $AnimatedSprite2D
+@onready var shape:CollisionShape2D = $CollisionShape2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(pop)
+
+func pop(body: Node2D):
+	audio.play()
+	sprite.visible = false
+	shape.set_deferred("disabled", true)
+	await audio.finished
+	queue_free()
