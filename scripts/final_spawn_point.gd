aaa6d569418bd9d8369d8b7ca4b73d53547d9375
Gaulent <jose.saucod@gmail.com>
2025-01-26 13:31:05 +0100

    Primera version más o menos

--- scripts/final_spawn_point.gd
+++ scripts/final_spawn_point.gd
@@ -5,17 +5,23 @@ extends Node2D
 @onready var shape_area:CollisionShape2D = $Area2D/CollisionShape2D
 @export var bubble_scene:PackedScene
 @export var small_bubble_scene:PackedScene
+@onready var dialogbox:Node2D = $DialogBox
+@onready var dialogtext:RichTextLabel = $DialogBox/PanelContainer/RichTextLabel
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	small_bubble_timer.timeout.connect(spawn_small_bubble)
-	trigger_area.body_entered.connect(spawn_bubble)
+	trigger_area.body_entered.connect(start_end_sequence)
 
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func spawn_bubble(body:Node2D) -> void:
+func start_end_sequence(body:Node2D) -> void:
 	LevelManagerSingleton.on_the_ending=true
-	await get_tree().create_timer(1.0).timeout
+	dialogbox.visible = true
+	var tween:= create_tween()
+	tween.tween_property(dialogtext, "visible_ratio", 1, 2).set_trans(Tween.TRANS_SINE)
+	await get_tree().create_timer(3.0).timeout
 	shape_area.disabled = true
 	var current_bubble:Node2D = bubble_scene.instantiate()
 	
